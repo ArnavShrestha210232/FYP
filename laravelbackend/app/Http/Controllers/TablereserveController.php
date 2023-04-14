93b0441a089c76091d8396e8b1b589c072a734c0
Arnav <arnavshresthaa@gmail.com>
2023-04-15 02:01:20 +0545

    Redoing the Tablereserves Controllers due to issues

--- laravelbackend/app/Http/Controllers/TablereserveController.php
+++ laravelbackend/app/Http/Controllers/TablereserveController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\tablereserve;
 use Illuminate\Http\Request;
 
+
 class TablereserveController extends Controller
 {
     /**
@@ -12,7 +13,8 @@ class TablereserveController extends Controller
      */
     public function index()
     {
-        //
+        $data = Tablereserve::latest()->paginate(5);
+        return view('tablereserve.index', compact('data'))->with('i',(request()->input('page',1)-1)*5);
     }
 
     /**
@@ -20,7 +22,7 @@ class TablereserveController extends Controller
      */
     public function create()
     {
-        //
+        return view('tablereserve.create');
     }
 
     /**
@@ -28,15 +30,45 @@ class TablereserveController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'time_in'         =>  'required',
+            'time_out'         =>  'required',
+            'made_by'         =>  'required',
+            'guests_number'         =>  'required',
+            'table_number'         =>  'required|unique:tablereserve',
+            'table_type'         =>  'required',
+            'contact_information'         =>  'required', 
+            'special_requests'         =>  'required',
+            'notes_comments'         =>  'required',
+        ]); 
+        
+
+        
+        $tablereserve = new Tablereserve;
+
+        $tablereserve->time_in = $request->time_in;
+        $tablereserve->time_out = $request->time_out;
+        $tablereserve->made_by = $request->made_by;
+        $tablereserve->guests_number = $request->guests_number;
+        $tablereserve->table_number = $request->table_number;
+        $tablereserve->table_type = $request->table_type;
+        $tablereserve->contact_information = $request->contact_information;
+        $tablereserve->special_requests = $request->special_requests;
+        $tablereserve->notes_comments = $request->notes_comments;
+
+        $tablereserve->save();
+
+        return redirect()->route('tablereserve.index')->with('success','Table Info Added Successfully.');
     }
 
     /**
      * Display the specified resource.
+     * @param  \App\Models\tablereserve  $tablereserve
+     * @return \Illuminate\Http\Response
      */
     public function show(tablereserve $tablereserve)
     {
-        //
+        return view('tablereserve.show', compact('tablereserve'));
     }
 
     /**
@@ -44,7 +76,7 @@ class TablereserveController extends Controller
      */
     public function edit(tablereserve $tablereserve)
     {
-        //
+        return view('tablereserve.edit', compact('tablereserve'));
     }
 
     /**
@@ -52,7 +84,34 @@ class TablereserveController extends Controller
      */
     public function update(Request $request, tablereserve $tablereserve)
     {
-        //
+        $request->validate([
+            'time_in'         =>  'required',
+            'time_out'         =>  'required',
+            'made_by'         =>  'required',
+            'guests_number'         =>  'required',
+            'table_number'         =>  'required|unique:tablereserve',
+            'table_type'         =>  'required',
+            'contact_information'         =>  'required', 
+            'special_requests'         =>  'required',
+            'notes_comments'         =>  'required',
+        ]); 
+
+
+        $tablereserve = tablereserve::find($request->hidden_id);
+
+        $tablereserve->time_in = $request->time_in;
+        $tablereserve->time_out = $request->time_out;
+        $tablereserve->made_by = $request->made_by;
+        $tablereserve->guests_number = $request->guests_number;
+        $tablereserve->table_number = $request->table_number;
+        $tablereserve->table_type = $request->table_type;
+        $tablereserve->contact_information = $request->contact_information;
+        $tablereserve->special_requests = $request->special_requests;
+        $tablereserve->notes_comments = $request->notes_comments;
+
+        $tablereserve->save();
+
+        return redirect()->route('tablereserve.index')->with('success', 'Table Info has been updated successfully');
     }
 
     /**
@@ -60,6 +119,9 @@ class TablereserveController extends Controller
      */
     public function destroy(tablereserve $tablereserve)
     {
-        //
+        $tablereserve->delete();
+
+        return redirect()->route('tablereserve.index')->with('success', 'Table Info has been deleted successfully');
     }
 }
+
