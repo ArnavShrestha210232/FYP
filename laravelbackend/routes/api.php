8aa48ab0b4f048396406438faf595e9e0b35d3aa
Arnav <arnavshresthaa@gmail.com>
2023-04-15 02:06:11 +0545

    for flutter connection API

--- laravelbackend/routes/api.php
+++ laravelbackend/routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AuthController;
 
 // Public Routes
+Route::post('/auth/register',[AuthController::class,'register'])->name('auth.register');
+Route::post('/auth/login',[AuthController::class,'login']);
+
+Route::middleware('auth:sanctum')->get('/user',function (Request $request){
+    return $request->user();
+});
+
 Route::get('/student',[StudentController::class, 'index']);
