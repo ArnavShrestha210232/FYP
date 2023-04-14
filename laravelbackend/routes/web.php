cea9d11a6b3eb45db3c3f9c551d2b8b0c214d3d5
Arnav <arnavshresthaa@gmail.com>
2023-04-15 02:05:37 +0545

    Web Panel for Admin

--- laravelbackend/routes/web.php
+++ laravelbackend/routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\FoodController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TableinfoController;
+use App\Http\Controllers\TablereserveController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,3 +34,9 @@ Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::resource ('food', FoodController::class);
 
 Route::resource ('tableinfo', TableinfoController::class);
+
+Route::resource ('tablereserve', TablereserveController::class);
+
+
+Route::post('/auth/register',[AuthController::class,'register'])->name('auth.register');
+Route::get('/auth/register',[AuthController::class,'show']);
