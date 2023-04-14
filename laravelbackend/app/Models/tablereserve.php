999f7a68f8acea809833532af675b92fcf76a3cb
Arnav <arnavshresthaa@gmail.com>
2023-04-15 02:03:24 +0545

    Modelling

--- laravelbackend/app/Models/tablereserve.php
+++ laravelbackend/app/Models/tablereserve.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tablereserve extends Model
+class Tablereserve extends Model
 {
+    protected $table = 'tablereserve';
     use HasFactory;
+    protected $fillable = ['time_in','time_out','made_by','guests_number','table_number', 'table_type', 'contact_information', 'special_requests','notes_comments'];
+
 }
